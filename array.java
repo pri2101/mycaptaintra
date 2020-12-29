bff028b329c1edfdbe0dd0363699dcce6f22dfcd
pri2101 <75296468+pri2101@users.noreply.github.com>
2020-12-29 15:07:34 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,19 @@
+public class array{
+    public static void main(String args[])
+    {
+        int a[][]= {{1,3,4},{2,4,3},{3,4,5}};
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+        int sum[][]= new int[3][3];
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                sum[i][j]=0;
+                sum[i][j]=a[i][j]+b[i][j];
+                System.out.print(sum[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
