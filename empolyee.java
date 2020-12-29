49381149506bc43e0223979ddd4bc85a9470fb51
pri2101 <75296468+pri2101@users.noreply.github.com>
2020-12-29 15:47:13 +0530

    create empolyee

---
+++ empolyee.java
@@ -0,0 +1,12 @@
+public class empolyee{
+
+    void showData()
+    {
+        System.out.println("Name\tYear of joining\tAddress\nRobert\t1994\t64-C-WallsStreet\nSam \t2000\t68D-WallsStreet\njohn\t1999\t26B-wallsStreet");
+    }
+    public static void main(String args[])
+    {
+        empolyee e=new empolyee();
+        e.showData();
+    }
+}
